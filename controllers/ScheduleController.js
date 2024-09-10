4290dd21ddb8c1d946206475cd4671af9bef6892
OB1LAB <ob1chaam@gmail.com>
2024-09-11 00:06:35 +0300

    1.11

--- controllers/ScheduleController.js
+++ controllers/ScheduleController.js
@@ -1,21 +1,23 @@
 import * as fs from "fs";
 import libs from "../libs.js";
 class ScheduleController {
+  schedule = JSON.parse(fs.readFileSync(`schedule.json`).toString());
   async get(req, res, next) {
     try {
-      res.json(schedule);
+      res.json(this.schedule);
     } catch (e) {
       next(e);
     }
   }
   async getDifference(req, res, next) {
     try {
-      res.json(await libs.generateDifference());
+      const data = await libs.generateDifference();
+      this.schedule = JSON.parse(fs.readFileSync(`schedule.json`).toString());
+      res.json(data);
     } catch (e) {
       next(e);
     }
   }
 }
 
-const schedule = JSON.parse(fs.readFileSync(`schedule.json`).toString());
 export default new ScheduleController();
